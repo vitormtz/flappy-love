ab17fa0786c00bba5d1e9632104f3c87b525c467
vitormtz <vitor.brak@hotmail.com>
2025-12-22 21:47:15 -0300

    chore: add maven repositories for dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,11 +11,17 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+
+        flatDir {
+            dirs("app/libs")
+        }
     }
 }
 
